e1db01c339e53a9a348859cae90a048a2e429ebf
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-10-08 22:36:27 +0200

    added getAllCoaches function

--- people-service/src/main/java/com/blf/people/controller/CoachController.java
+++ people-service/src/main/java/com/blf/people/controller/CoachController.java
@@ -1,4 +1,24 @@
 package com.blf.people.controller;
+import com.blf.people.dao.CoachDao;
+import com.blf.people.entity.Coach;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/coach")
+@AllArgsConstructor
+@Slf4j
 public class CoachController {
+
+    CoachDao coachDao;
+
+    @GetMapping("")
+    public List<Coach> getAllCoaches(){
+        return coachDao.getAllCoaches();
+    }
 }
