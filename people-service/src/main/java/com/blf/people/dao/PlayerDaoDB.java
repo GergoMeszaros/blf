b20e2cc75b84e3b238a3c4ade786be1ca1bbf836
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-10-09 22:34:36 +0200

    created people-service module

--- people-service/src/main/java/com/blf/people/dao/PlayerDaoDB.java
+++ people-service/src/main/java/com/blf/people/dao/PlayerDaoDB.java
@@ -61,5 +61,4 @@ public class PlayerDaoDB implements PlayerDao {
         return playerToDelete;
     }
 
-
 }
--- people-service/src/main/java/com/blf/people/repository/PlayerRepository.java
+++ people-service/src/main/java/com/blf/people/repository/PlayerRepository.java
@@ -3,4 +3,5 @@ import com.blf.people.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+
 }
