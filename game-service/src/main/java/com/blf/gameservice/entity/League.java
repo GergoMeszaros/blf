b3dc8d993ef89f6c4aa8a8eae01f7585bcf5003d
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-10-10 10:44:45 +0200

    - added jpa to pom.xml
    - annotated Season and League classes

--- game-service/src/main/java/com/blf/gameservice/entity/League.java
+++ game-service/src/main/java/com/blf/gameservice/entity/League.java
@@ -1,4 +1,28 @@
 package com.blf.gameservice.entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class League {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+    String organization;
+    int seasonId;
+    int ageGroupId;
+
 }
--- game-service/src/main/java/com/blf/gameservice/entity/Season.java
+++ game-service/src/main/java/com/blf/gameservice/entity/Season.java
@@ -1,4 +1,23 @@
 package com.blf.gameservice.entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Season {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String date;
 }
