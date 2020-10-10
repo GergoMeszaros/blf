a166cac26a5fd99621c44d7b47dff8c9496b47bd
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-10-10 13:07:17 +0200

    added GameController class

---
+++ game-service/src/main/java/com/blf/gameservice/controller/GameController.java
@@ -0,0 +1,14 @@
+package com.blf.gameservice.controller;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/game")
+@AllArgsConstructor
+@Slf4j
+public class GameController {
+
+
+}
