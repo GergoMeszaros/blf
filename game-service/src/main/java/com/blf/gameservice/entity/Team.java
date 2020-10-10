981ba19b2f872c26e0db2349167cc2da172f445b
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-10-10 11:05:38 +0200

    annotated Team class

--- game-service/src/main/java/com/blf/gameservice/entity/Team.java
+++ game-service/src/main/java/com/blf/gameservice/entity/Team.java
@@ -1,4 +1,38 @@
 package com.blf.gameservice.entity;
 
+import com.sun.xml.bind.v2.TODO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    int seasonId;
+    //int ageGroupId;
+    int coachId;
+    int assistantCoachId;
+    int leagueId;
+
+    String name;
+    String address;
+
+    //Todo (megjegyzés, age_group, blf??)
+    //String note;
+    //int blf;
+
 }
