55ff55161b562ec39e2dc038b9c7f0bc632841ba
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-11-13 10:04:22 +0100

    deleted unused imports from TeamMemberServiceCaller.java

--- game-service/src/main/java/com/blf/gameservice/servicecaller/TeamMemberServiceCaller.java
+++ game-service/src/main/java/com/blf/gameservice/servicecaller/TeamMemberServiceCaller.java
@@ -1,16 +1,10 @@
 package com.blf.gameservice.servicecaller;
 import com.blf.gameservice.dto.PlayerDTO;
 import com.blf.gameservice.dto.PlayerWrapper;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
