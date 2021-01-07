908810b926f35e6a46173ee1ce9bb783ce990106
gergo1212 <gergo.meszaros.codecool@gmail.com>
2021-01-07 15:31:16 +0100

    - added description to UpdateValidator's updater() method

--- game-service/src/main/java/com/blf/gameservice/service/UpdateValidator.java
+++ game-service/src/main/java/com/blf/gameservice/service/UpdateValidator.java
@@ -5,9 +5,18 @@ import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Field;
 
+
 @Service
 public class UpdateValidator {
 
+    /**
+     * This method is using generics and to accept only the required class types, it extends
+        the parent class of all entities.
+     * Firstly it creates two arrays from the fields of the parameter objects.
+     * After that the loop makes the values accessible regardless of their access modifier.
+     * Finally compares the values of the fields and only if these values are different and not named as "id" and
+        the updatedField's value is not "null" replaces the old vale.
+     */
     protected <Type extends DataModel> Type updater(Type old, Type input) throws IllegalAccessException {
 
         Field[] oldFields = old.getClass().getDeclaredFields();
