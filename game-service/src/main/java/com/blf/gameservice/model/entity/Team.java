ea46eb95d0a28334b7713c80c3fa126d10631c28
gergo1212 <gergo.meszaros.codecool@gmail.com>
2021-01-30 08:18:17 +0100

    Team has a new field isBlfTeam which is transient

--- game-service/src/main/java/com/blf/gameservice/model/entity/Team.java
+++ game-service/src/main/java/com/blf/gameservice/model/entity/Team.java
@@ -1,8 +1,14 @@
 package com.blf.gameservice.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @EqualsAndHashCode(callSuper = false)
 @Data
@@ -12,12 +18,33 @@ import javax.persistence.*;
 @Builder
 public class Team extends BaseEntity {
 
-
+    @JsonIgnore
     private Integer blf;
+
+    @Transient
+    private String isBlfTeam;
 
     private String name;
     private String address;
 
     @ManyToOne
     private Season season;
+
+
+    /*@OneToMany
+    @JoinTable(
+            name = "team_member",
+            joinColumns = @JoinColumn(name = "id"),
+            inverseJoinColumns = @JoinColumn(name = "team_id"))
+    private Set<TeamMember> teamMember;*/
+
+
+    @PostLoad
+    private void setIsBlf() {
+        if (blf == 1) {
+            isBlfTeam = "Blf";
+        } else {
+            isBlfTeam = "";
+        }
+    }
 }
